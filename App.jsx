aac63f9e14fdb75f4230510e6bb3d0a9e4d31a8f
Chips <125469898+Chipsii@users.noreply.github.com>
2023-07-04 13:27:59 +0600

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react"
+import "./styles.css"
+
+export default function App() {
+  const [newItem, setNewItem] = useState("")
+
+  return (
+  <>
+  <form className="new-item-form">
+    <div className="form-row">
+      <label htmlFor="item">New Item </label>
+      <input value={newItem} onChange={e => setNewItem(e.target.value)} type="text" id="item"/>
+    </div>
+    <button className="btn">Add</button>
+  </form>
+  <h1 className="header">To do List</h1>
+  <ul className="list">
+    <li>
+      <label>
+        <input type="checkbox"/>
+          Item 1
+      </label>
+      <button className="btn-delete">Delete</button>
+    </li>
+    <li>
+      <label>
+        <input type="checkbox"/>
+          Item 2
+      </label>
+      <button className="btn-delete">Delete</button>
+    </li>
+  </ul>
+  </>
+  )
+}
